18e16920870aaee789394693b13116624f111473
KinoLien <miecowbai@gmail.com>
2019-07-08 17:33:41 +0800

    First commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var passport = require('passport');
+
+var isDev = process.env.NODE_ENV === 'development';
+
+if (isDev) {
+}
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// require('./config/express')(app, passport);
+
+// require('./config/routes')(app, passport);
+
+var server = http.createServer(app).listen(app.get('port'));
+
